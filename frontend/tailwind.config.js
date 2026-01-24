de8c03a4e78ba40e43001e57f10b818d5242ccbb
Wilfred007 <adzerwilfred007@gmail.com>
2026-01-24 06:18:48 +0100

    Tailwind css added

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/*********************
+ * Tailwind CSS Config
+ *********************/
+module.exports = {
+  content: [
+    "./src/app/**/*.{ts,tsx}",
+    "./src/components/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
